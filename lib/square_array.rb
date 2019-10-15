0bbcee93dceca4dfeda105d01ceb32f11efc7ce1
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-15 02:57:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,11 @@
 def square_array(numbers)
   counter = 0 
+  extra_array = []
   
   while numbers[counter] do
-    return numbers[counter] **2
+    array_numbers = numbers[counter] **2
+    extra_array << array_numbers
     counter += 1
   end
+  p extra_array
 end
